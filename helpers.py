e4e3355e09a68b3c50e9e4ddb15a68f0a8310caa
keshav nagpal <keshav@gravityilabs.com>
2019-07-28 19:30:41 +0530

    initial commit

---
+++ helpers.py
@@ -0,0 +1,69 @@
+import json
+import datetime as dt
+import redis
+import httplib2
+from io import BytesIO
+from zipfile import ZipFile
+import pandas as pd
+
+r = redis.StrictRedis(host='sdztest.redis.cache.windows.net',
+        port=6380, db=0, password='4vjBFdTGgyiiefz4fvUZRqQ65L5wNFmnHxowVRylBKc=', ssl=True)
+
+def DownloadBSEFile():	
+	URL = 'https://www.bseindia.com/download/BhavCopy/Equity/'
+
+	today = dt.datetime.today()
+
+	i=1
+	while(True):
+		
+		day = today.day
+		if(day<10):
+			day = '0'+str(day)
+		month = today.month
+		if(month<10):
+			month = '0'+str(month)
+		year = today.year
+
+		fileName = 'EQ'+str(day)+str(month)+str(year)[2:4]+'_CSV.ZIP'
+		FileURL = URL + fileName
+		h = httplib2.Http(".cache")
+		resp, content = h.request(FileURL, "GET")
+		if(resp['status']!='404'):
+			r.set("FileDate", today.strftime('%d %b %Y'))
+			break
+		today = today - dt.timedelta(i)
+	return content
+
+def extractZipFile(resp):
+	zipfile = ZipFile(BytesIO(resp))
+	#print(zipfile.namelist())
+	zipfile.extract(zipfile.namelist()[0])
+	stockdf = pd.read_csv(zipfile.namelist()[0])
+	return stockdf
+
+def saveFieldsToRedis(stockdf):
+	stockCode = list(stockdf.SC_CODE)
+	stockName = list(stockdf.SC_NAME)
+	stockHigh = list(stockdf.HIGH)
+	stockLow = list(stockdf.LOW)
+	stockOpen = list(stockdf.OPEN)
+	stockClose = list(stockdf.CLOSE)
+	stockDiffFromPrevClose = list((stockdf.CLOSE - stockdf.PREVCLOSE)/stockdf.CLOSE)
+	print(stockDiffFromPrevClose)
+	stockDict = {
+		'code':stockCode,
+		'name':stockName,
+		'high':stockHigh,
+		'low':stockLow,
+		'open':stockOpen,
+		'close':stockClose,
+		'change':stockDiffFromPrevClose,
+		'FileDate': r.get('FileDate').decode()
+		}
+	stockJson = json.dumps(stockDict)
+	r.set("stockJson",stockJson)
+
+def getFieldsFromRedis():	
+	stockJson = r.get("stockJson")
+	return stockJson
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, flash, redirect, render_template, g, Response, request, session, abort, jsonify
+import json
+from flask_cors import CORS
+from logger import logger
+import helpers as HM
+from flask_caching import Cache
+from flask_compress import Compress
+
+cachetime = 100
+app = Flask(__name__)
+#This is for the app cache, not the bse data cache in redis
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
+Compress(app)
+
+# To accept requests from any domain / IP
+CORS(app)
+
+@app.route('/')
+def hello_world():
+	return 'You are a curious fellow!'
+
+# since the files get updated everyday, this could be moved to a scheduled webjob
+@cache.memoize(timeout=10000)
+@app.route('/bse')
+def bse():
+	# DownloadAndSaveBSEToRedis()
+	BSEFile = HM.DownloadBSEFile()
+	stockdf = HM.extractZipFile(BSEFile)
+	HM.saveFieldsToRedis(stockdf)
+	return render_template('bse.html')
+
+@app.route("/getBSEData", methods=['GET'])
+def getBSEData():		
+	return HM.getFieldsFromRedis()
+
+ 
+if __name__ == '__main__':
+	app.run(debug=True)
