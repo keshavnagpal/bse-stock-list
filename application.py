acc2b41617bb7543bb1f95e1581a42efa8bf8636
keshav nagpal <keshav@gravityilabs.com>
2019-08-08 16:22:10 +0530

    Revert "Merge pull request #1 from keshavnagpal/cherrypy"
    This reverts commit e93e92a89eab9b8e3bae92d6320cc28c20cfc96f, reversing
    changes made to 938a1a09d3153bf74dab9c9e16d81f2f7ae5c014.

--- application.py
+++ application.py
@@ -1,36 +1,37 @@
-import cherrypy
+from flask import Flask, flash, redirect, render_template, g, Response, request, session, abort, jsonify
 import json
+from flask_cors import CORS
 import helpers as HM
-import os
+from flask_caching import Cache
+from flask_compress import Compress
 
-class Root(object):
-	@cherrypy.expose
-	def index(self):		
-		return "You're a curious fellow!"
+cachetime = 100
+app = Flask(__name__)
+#This is for the app cache, not the bse data cache in redis
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
+Compress(app)
 
-	@cherrypy.expose
-	def bse(self):
-		BSEFile = HM.DownloadBSEFile()
-		stockdf = HM.extractZipFile(BSEFile)
-		HM.saveFieldsToRedis(stockdf)
-		return open("templates/bse.html")
+# To accept requests from any domain / IP
+CORS(app)
 
-	@cherrypy.expose
-	@cherrypy.tools.response_headers(headers=[('Content-Type', 'application/json')])
-	def getBSEData(self):		
-		return HM.getFieldsFromRedis()
+@app.route('/')
+def hello_world():
+	return 'You are a curious fellow!'
 
+# since the files get updated everyday, this could be moved to a scheduled webjob
+@cache.memoize(timeout=10000)
+@app.route('/bse')
+def bse():
+	# DownloadAndSaveBSEToRedis()
+	BSEFile = HM.DownloadBSEFile()
+	stockdf = HM.extractZipFile(BSEFile)
+	HM.saveFieldsToRedis(stockdf)
+	return render_template('bse.html')
 
-CP_CONF = {
-	'/media': {
-		'tools.staticdir.on': True,
-		'tools.staticdir.dir': os.path.abspath('./') # staticdir needs an absolute path
-		}
-	}
-app = cherrypy.Application(Root(),'/', CP_CONF)
+@app.route("/getBSEData", methods=['GET'])
+def getBSEData():		
+	return HM.getFieldsFromRedis()
 
+ 
 if __name__ == '__main__':
-	from wsgiref.simple_server import make_server
-
-	httpd = make_server('', 5000, app)
-	httpd.serve_forever()
+	app.run(debug=True)
